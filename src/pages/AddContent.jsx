3cc4e70aeb1b34671879d993e0b76f733bab1449
jafril <jafril.adroittechnology@gmail.com>
2025-12-30 14:14:23 +0530

    Add ViewContent component to display posts

---
+++ src/pages/AddContent.jsx
@@ -0,0 +1,14 @@
+const ViewContent = () => {
+  return (
+    <div className="card">
+      <h2>View Content</h2>
+      <ul>
+        <li>Post 1</li>
+        <li>Post 2</li>
+        <li>Post 3</li>
+      </ul>
+    </div>
+  );
+};
+
+export default ViewContent;
